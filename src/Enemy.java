316efdb75efff8036ec8e342a4e3807133abb523
Zachary Hansen Terry <zmht25@gmail.com>
2022-04-02 10:49:36 -0500

    ggez finished

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,20 +1,28 @@
 import java.awt.*;
 
 public class Enemy extends Entity{
-    int tolerance = 100;
+
     public Enemy(int x, int y, int w, int h) {
         super(x, y, w, h);
         yVel = 0;
         xVel = 0;
     }
 
+    long ticks = 0;
     @Override
     public void update() {
-        if(yPos < Game.ball.yPos){
+        ticks++;
+        if(yPos < Game.ball.yPos && ticks % 40 == 0){
             yVel = 10;
         }
-        if(yPos > Game.ball.yPos){
+        if(yPos > Game.ball.yPos && ticks % 40 == 0){
             yVel = -10;
+        }
+        if(xPos < 0){
+            xPos = 0;
+        }
+        if(xPos + height > Game.HEIGHT){
+            xPos = Game.HEIGHT;
         }
         xPos += xVel;
         yPos += yVel;
